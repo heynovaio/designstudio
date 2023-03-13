ccae6eee5ba0ff3a2c42b8bec8b946656c361442
Klancashire <kathryn@klancashire.com>
2023-03-13 14:42:38 -0700

    Mobile Menu woo hoo

--- src/components/menus/TopMenu.js
+++ src/components/menus/TopMenu.js
@@ -3,10 +3,18 @@ import { Link, graphql } from 'gatsby'
 import { PrismicLink, PrismicText } from '@prismicio/react'
 import { GatsbyImage, StaticImage } from 'gatsby-plugin-image'
 import { Container } from './../Components'
-import { BiShoppingBag, BiHeart, BiSearch } from 'react-icons/bi'
+import { BiShoppingBag, BiHeart, BiSearch, BiMenu, BiXCircle } from 'react-icons/bi'
 
 import * as sty from './TopMenu.module.scss'
 export const TopMenu = ({ menu, activeDocMeta }) => {
+  const [mobileMenu, setMobileMenu] = React.useState(false)
+
+  const toggleMenu = () => {
+    setMobileMenu(!mobileMenu);
+    document.body.classList.remove('modal-open');
+    document.body.classList.toggle('modal-open', !mobileMenu);
+  }
+
   return (
     <header className={sty.header}>
       <Container>
@@ -21,7 +29,7 @@ export const TopMenu = ({ menu, activeDocMeta }) => {
               className={sty.logo}
             />
           </PrismicLink>
-          <div className={sty.NavWrap}>
+          <div className={`${sty.NavWrap} ${mobileMenu? sty.navOpen : sty.navClosed}`}>
             <div className={sty.MenuLinks}>
               {menu.menu_links.map((item, index) => (
                 <PrismicLink href={item.link?.url} key={`menuLink:${index}`}>
@@ -29,7 +37,30 @@ export const TopMenu = ({ menu, activeDocMeta }) => {
                 </PrismicLink>
               ))}
             </div>
+            <div className={sty.SocialGroup}>
+              <ul className={`list-no-style ${sty.SocialLinks}`}>
+                {menu.socials.map((item, index) => (
+                  <li>
+                    <PrismicLink
+                      href={'/'}
+                      key={`socialLink:${index}`}
+                      className={sty.SocialLink}
+                    >
+                      <GatsbyImage
+                        image={item?.social_icon?.gatsbyImageData}
+                        alt={item.social_icon?.alt || ''}
+                      />
+                    </PrismicLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className={sty.iconNav}>
             <NavBarIcons />
+            <button onClick={toggleMenu} className={`${sty.mobileBtn}  ${mobileMenu? sty.navOpen : sty.navClosed}`}>
+              {mobileMenu? (<BiXCircle size={25} />) : (<BiMenu size={25} />)}
+            </button>
           </div>
         </div>
       </Container>
