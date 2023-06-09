8e9110654599a211f9a2f9cb6b1f69b1076b9d08
AveryDodd <averyhvdodd@gmail.com>
2023-06-08 17:17:26 -0700

    another map fix attempt

--- src/components/_homepage/contact.js
+++ src/components/_homepage/contact.js
@@ -3,9 +3,13 @@ import { GatsbyImage, StaticImage } from 'gatsby-plugin-image'
 import { PrismicRichText, PrismicText, PrismicLink } from '@prismicio/react'
 import { Container, Button } from '../Components'
 import Map, { Marker, Popup } from 'react-map-gl'
+
+import mapboxgl from 'mapbox-gl';
+// The following is required to stop "npm build" from transpiling mapbox code.
+// notice the exclamation point in the import.
 // @ts-ignore
-// eslint-disable-next-line import/no-webpack-loader-syntax
-import mapboxgl from '!mapbox-gl';
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
   
 import {
   FaMapMarkerAlt,
