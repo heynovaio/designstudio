d845e1134c78e35fd91cc5e8e67a5da29a45e50c
Karlee Milbury <karleemilbury@gmail.com>
2024-06-12 13:50:10 -0300

    quote nav control styling

--- src/components/_homepage/testimonials.js
+++ src/components/_homepage/testimonials.js
@@ -25,29 +25,7 @@ export const Testimonials = ({ testimonials, background }) => {
         style={{ background: background }}
       >
         <Container className={sty.flex}>
-          <div className={sty.quoteNav}>
-            <div className={sty.controlWrap}>
-              <div className={sty.quoteControls}>
-                <div>
-                  <b>{slide + 1}</b> of {slideLength}
-                </div>
-                <div className={sty.dotList}>
-                  {testimonials.map((item, index) => (
-                    <div
-                      className={index === slide ? sty.activeDot : sty.dot}
-                      onClick={() => {
-                        setSlide(index)
-                      }}
-                      key={`dot:${index}`}
-                    />
-                  ))}
-                </div>
-              </div>
-              <div className={sty.nextBtn} onClick={handleClick}>
-                <FaChevronRight />
-              </div>
-            </div>
-          </div>
+          <div className={sty.quoteNav}></div>
 
           <div className={sty.imageWrap}>
             <Parallax translateY={[-20, 10]}>
@@ -75,6 +53,27 @@ export const Testimonials = ({ testimonials, background }) => {
                 <span className={sty.author}>{item.testimonial_author}</span>
               </div>
             ))}
+            <div className={sty.controlWrap}>
+              <div className={sty.quoteControls}>
+                <div>
+                  <b>{slide + 1}</b> of {slideLength}
+                </div>
+                <div className={sty.dotList}>
+                  {testimonials.map((item, index) => (
+                    <div
+                      className={index === slide ? sty.activeDot : sty.dot}
+                      onClick={() => {
+                        setSlide(index)
+                      }}
+                      key={`dot:${index}`}
+                    />
+                  ))}
+                </div>
+              </div>
+              <div className={sty.nextBtn} onClick={handleClick}>
+                <FaChevronRight />
+              </div>
+            </div>
           </div>
         </Container>
       </div>
