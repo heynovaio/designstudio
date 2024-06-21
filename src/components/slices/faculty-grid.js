c15f8ad76db350a3baf675d2073e8bd15a2541cb
Karlee Milbury <karleemilbury@gmail.com>
2024-06-21 14:11:31 -0300

    Removed bios

--- src/components/slices/faculty-grid.js
+++ src/components/slices/faculty-grid.js
@@ -1,74 +1,67 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
-import { GatsbyImage, StaticImage } from 'gatsby-plugin-image'
-import { PrismicRichText,PrismicLink } from '@prismicio/react'
-import { Container, Button } from "../Components"
-import { IoClose } from "react-icons/io5"
-import * as sty from './faculty-grid.module.scss';
+import { GatsbyImage } from 'gatsby-plugin-image'
+import { PrismicRichText } from '@prismicio/react'
+import { Container, Button } from '../Components'
+import { IoClose } from 'react-icons/io5'
+import * as sty from './faculty-grid.module.scss'
 
 export const FacultyGrid = ({ slice }) => {
-  const [activeBio, setActiveBio] = React.useState(-1);
+  const [activeBio, setActiveBio] = React.useState(-1)
 
   const handleClick = (e) => {
-    setActiveBio(e.target.value);
-  
+    setActiveBio(e.target.value)
+
     if (typeof document !== `undefined`) {
-      document.body.classList.toggle('modal-open');
+      document.body.classList.toggle('modal-open')
     }
   }
   const handleClose = (e) => {
-    setActiveBio(-1);
+    setActiveBio(-1)
     if (typeof document !== `undefined`) {
-      document.body.classList.remove('modal-open');
+      document.body.classList.remove('modal-open')
     }
   }
   return (
     <section className={sty.FacultyGrid}>
       <Container>
-        <div style={{marginBottom: 60}}>
+        <div style={{ marginBottom: 60 }}>
           <h2>{slice.primary.title}</h2>
         </div>
         <div className={sty.grid}>
-          {slice.items.map((item,index) => (
+          {slice.items.map((item, index) => (
             <div className={sty.employeeCard}>
               <GatsbyImage
                 image={item.image?.gatsbyImageData}
-                alt={item.image?.alt || ""}
+                alt={item.image?.alt || ''}
                 className={sty.image}
                 imgStyle={{ objectPosition: 'right 20%' }}
-                />
+              />
               <div className={sty.copyWrap}>
                 <span className={sty.name}>{item.name}</span>
-                <p className={sty.subName}>
-                  Favourite Style: {item.favourite_style}
-                </p>
-                <p style={{marginBottom: 20}}>{item.mini_bio}</p>
-                <Button>
-                  <button onClick={handleClick} value={index}>Read More About {item.name}</button>
-                </Button>
-               
               </div>
             </div>
           ))}
         </div>
-        {activeBio != -1 && ( 
-          <div className={sty.bioModalWrap} >
+        {activeBio != -1 && (
+          <div className={sty.bioModalWrap}>
             <div className={sty.bioModal}>
               <div className={sty.copyWrap}>
                 <div className={sty.close} onClick={handleClose}>
-                  <IoClose/>
+                  <IoClose />
                 </div>
                 <span className={sty.name}>{slice.items[activeBio]?.name}</span>
                 <p className={sty.subName}>
                   Favourite Style: {slice.items[activeBio]?.favourite_style}
                 </p>
-                <PrismicRichText field={slice.items[activeBio]?.bio?.richText}/>
-              
+                <PrismicRichText
+                  field={slice.items[activeBio]?.bio?.richText}
+                />
               </div>
               <div className={sty.imageWrapModal}>
                 <GatsbyImage
                   image={slice.items[activeBio]?.image?.gatsbyImageData}
-                  alt={slice.items[activeBio]?.image?.alt || ""}
+                  alt={slice.items[activeBio]?.image?.alt || ''}
                   className={sty.image}
                 />
               </div>
@@ -91,7 +84,7 @@ export const query = graphql`
         gatsbyImageData
         alt
       }
-      name 
+      name
       favourite_style
       mini_bio
       bio {
