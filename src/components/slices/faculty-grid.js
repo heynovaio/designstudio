563734768aa824f9148d3839ca5da471d1867169
Karlee Milbury <karleemilbury@gmail.com>
2024-06-21 19:32:47 -0300

    styled employee card layout + removed unused class styles

--- src/components/slices/faculty-grid.js
+++ src/components/slices/faculty-grid.js
@@ -17,22 +17,20 @@ export const FacultyGrid = ({ slice }) => {
             image={slice.primary.faculty_header_image?.gatsbyImageData}
             alt={slice.primary.faculty_header_image?.alt || ''}
             className={sty.image}
-            imgStyle={{ objectPosition: 'right 20%' }}
           />
         </div>
 
         <div className={sty.grid}>
-          {slice.items.map((item, index) => (
+          {slice.items.map((item) => (
             <div className={sty.employeeCard}>
-              <GatsbyImage
-                image={item.image?.gatsbyImageData}
-                alt={item.image?.alt || ''}
-                className={sty.image}
-                imgStyle={{ objectPosition: 'right 20%' }}
-              />
-              <div className={sty.copyWrap}>
-                <span className={sty.name}>{item.name}</span>
+              <div className={sty.imageWrap}>
+                <GatsbyImage
+                  image={item.image?.gatsbyImageData}
+                  alt={item.image?.alt || ''}
+                  className={sty.image}
+                />
               </div>
+              <span className={sty.name}>{item.name}</span>
             </div>
           ))}
         </div>
