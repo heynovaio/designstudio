36472ac8b39567b0e0b61ab03daf21dd0dbbea89
Kathryn Lancashire <kathryn@klancashire.com>
2024-06-21 11:15:46 -0700

    Further styling and non-home config

--- src/components/slices/hero-banner.js
+++ src/components/slices/hero-banner.js
@@ -5,27 +5,41 @@ import { PrismicRichText, PrismicLink } from '@prismicio/react'
 import { Container } from '../Components'
 import { Button } from '../Components'
 
-import * as sty from './text-image.module.scss'
+import * as sty from './hero-banner.module.scss'
 
 export const HeroBanner = ({ slice }) => {
+
   return (
-    <section className={sty.HeroBanner} style={{ paddingBottom: 0 }}>
-      <Container className="flex">
+    <section className={sty.heroBanner} style={{ paddingBottom: 0 }}>
+      <div className={`${sty.heroContainer} ${location.pathname === '/' ? sty.homeHero : ''}`}>
+      <div className={sty.logoWrap}>
+        <StaticImage
+          src="../../images/designstudio_logo.png"
+          width={378}
+          height={182}
+          layout="constrained"
+          className={sty.image}
+        />
+      </div>
         <div className={sty.copyWrap}>
           <PrismicRichText field={slice.primary.page_title?.richText} />
-          <PrismicLink href={'/#contactUs'} className={sty.BtnPrimary}>
-            Get in touch
-          </PrismicLink>
-        </div>
-        <div className={sty.imageWrap}>
-          {slice.primary.image && (
-            <GatsbyImage
-              image={slice.primary.image?.gatsbyImageData}
-              alt={slice.primary.image?.alt || ''}
-            />
+          {slice.primary.callout_link && (
+            <PrismicLink href={slice.primary.callout_link.url} className={sty.BtnPrimary}>
+              {slice.primary.callout_label}
+            </PrismicLink>
           )}
         </div>
-      </Container>
+        {slice.primary.hero_banner_image && (
+          <GatsbyImage
+            className={sty.heroImage}
+            image={slice.primary.hero_banner_image?.gatsbyImageData}
+            alt={slice.primary.hero_banner_image?.alt || ''}
+            objectFit='cover'
+            objectPosition='50% 50%'
+            loading='eager'
+            />
+        )}
+      </div>
     </section>
   )
 }
@@ -38,9 +52,29 @@ export const query = graphql`
         richText
       }
       hero_banner_image {
-        gatsbyImageData
+        gatsbyImageData(layout: FULL_WIDTH)
         alt
       }
+      callout_link {
+        url
+      }
+      callout_label
+    }
+  }
+  fragment PageDataBodyHeroBanner on PrismicPageDataBodyHeroBanner {
+    id
+    primary {
+      page_title {
+        richText
+      }
+      hero_banner_image {
+        gatsbyImageData(layout: FULL_WIDTH)
+        alt
+      }
+      callout_link {
+        url
+      }
+      callout_label
     }
   }
 `
