62e57bba32c7e2df2aad4f89d32730de1dde860a
Kathryn Lancashire <kathryn@klancashire.com>
2024-06-24 12:26:18 -0700

    A fix for Gatsbys issue with server side rendering and location

--- src/components/slices/hero-banner.js
+++ src/components/slices/hero-banner.js
@@ -5,23 +5,31 @@ import { PrismicRichText, PrismicLink } from '@prismicio/react'
 import {
   BiArrowBack
 } from 'react-icons/bi'
+import { useLocation } from '@reach/router'
 
 import * as sty from './hero-banner.module.scss'
 
 export const HeroBanner = ({ slice }) => {
+  const location = useLocation(); // Use useLocation hook to get location object
+  const [local, setLocal] = React.useState('/'); // Initialize local state
+
+  React.useEffect(() => {
+    // Set local state based on the current pathname
+    setLocal(location.pathname);
+  }, [location.pathname]); // Dependency array, effect runs only when pathname changes
 
   return (
     <section className={sty.heroBanner} style={{ paddingBottom: 0 }}>
-      <div className={`${sty.heroContainer} ${location.pathname === '/' ? sty.homeHero : ''}`}>
-      <div className={sty.logoWrap}>
-        <StaticImage
-          src="../../images/designstudio_logo.png"
-          width={378}
-          height={182}
-          layout="constrained"
-        />
-      </div>
-        <div className={`${sty.copyWrap} ${location.pathname === '/' ? sty.homeCopy : ''}`}>
+      <div className={`${sty.heroContainer} ${local === '/' ? sty.homeHero : ''}`}>
+        <div className={sty.logoWrap}>
+          <StaticImage
+            src="../../images/designstudio_logo.png"
+            width={378}
+            height={182}
+            layout="constrained"
+          />
+        </div>
+        <div className={`${sty.copyWrap} ${local === '/' ? sty.homeCopy : ''}`}>
           <PrismicRichText field={slice.primary.page_title?.richText} />
           {slice.primary.callout_link && (
             <PrismicLink href={slice.primary.callout_link.url} className={`BtnPrimary ${sty.HeroBtn}`}>
