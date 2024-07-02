639fa48ca62de02af562b37cd36d71e2b5cb5f29
Karlee Milbury <karleemilbury@gmail.com>
2024-07-02 10:52:19 -0300

    Added text-callout.js and scss file

---
+++ src/components/slices/text-callout.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import { GatsbyImage, StaticImage } from 'gatsby-plugin-image'
+import { PrismicRichText } from '@prismicio/react'
+import { Container, Button } from '../Components'
+import * as sty from './text-callout.module.scss'
+
+export const FacultyGrid = ({ slice }) => {
+  return <Container></Container>
+}
