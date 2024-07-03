7258ffe38d39967012558a1f9c5e9c9803491d6f
Karlee Milbury <karleemilbury@gmail.com>
2024-07-03 14:14:41 -0300

    Styled slice

--- src/components/slices/intro-description.js
+++ src/components/slices/intro-description.js
@@ -3,16 +3,43 @@ import { graphql } from 'gatsby'
 import { GatsbyImage, StaticImage } from 'gatsby-plugin-image'
 import { PrismicRichText } from '@prismicio/react'
 import { Container, Button } from '../Components'
+import * as sty from './intro-description.module.scss'
+import { BiArrowBack } from 'react-icons/bi'
 
 export const IntroDescription = ({ slice }) => {
   return (
-    <Container>
-      <PrismicRichText field={slice.primary.intro_description_title.richText} />
-      <PrismicRichText field={slice.primary.intro_description_text.richText} />
-      <PrismicRichText
-        field={slice.primary.intro_description_btn_label.richText}
-      />
-    </Container>
+    <div className={sty.introBox}>
+      <div className={sty.textBox}>
+        <div className={sty.titleText}>
+          <PrismicRichText
+            field={slice.primary.intro_description_title.richText}
+          />
+        </div>
+
+        <div className={sty.bodyText}>
+          <PrismicRichText
+            field={slice.primary.intro_description_text.richText}
+          />
+        </div>
+      </div>
+
+      <div className={sty.buttonArea}>
+        <button className={sty.viewWorkButton}>
+          <span className={sty.btnContent}>
+            <PrismicRichText
+              field={slice.primary.intro_description_btn_label.richText}
+            />
+            <BiArrowBack
+              size={18}
+              style={{
+                transform: 'scaleX(-1)',
+                marginLeft: '0.5em',
+              }}
+            />
+          </span>
+        </button>
+      </div>
+    </div>
   )
 }
 
