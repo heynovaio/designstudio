9fc8f697455d4a1d6a1ea5dd3216148534499863
Karlee Milbury <karleemilbury@gmail.com>
2024-09-11 11:08:20 -0300

    image styling for description and added backgrounds

--- src/components/_project/project-description.js
+++ src/components/_project/project-description.js
@@ -1,13 +1,27 @@
 import * as React from 'react'
 import { Container } from '../Components'
 import { PrismicRichText } from '@prismicio/react'
+import { GatsbyImage } from 'gatsby-plugin-image'
 
-import * as sty from './project-hero.module.scss'
+import * as sty from './project-description.module.scss'
 
-export const ProjectDescription = ({ description }) => {
+export const ProjectDescription = ({ description, gallery }) => {
   return (
     <section className={sty.ProjectDescription}>
       <Container>
+        {gallery && (
+          <div className={sty.galleryWrap}>
+            {gallery.map((item, index) => (
+              <div className={sty.imageWrap} key={`galleryItem: ${index}`}>
+                <GatsbyImage
+                  image={item.image?.gatsbyImageData}
+                  alt={item.image?.alt || ''}
+                  className={sty.image}
+                />
+              </div>
+            ))}
+          </div>
+        )}
         <div className={sty.Content}>
           <div className={sty.copyWrap}>
             <PrismicRichText field={description} />
