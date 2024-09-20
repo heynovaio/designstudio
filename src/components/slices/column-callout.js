3911721187b8102aeddb5b675498f7492ac8eb18
Karlee Milbury <karleemilbury@gmail.com>
2024-09-20 15:20:03 -0300

    Added images + image styling

--- src/components/slices/column-callout.js
+++ src/components/slices/column-callout.js
@@ -3,34 +3,37 @@ import { graphql } from 'gatsby'
 import { PrismicRichText, PrismicLink } from '@prismicio/react'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import * as sty from './column-callout.module.scss'
+import { Container } from '../Components'
 
 export const ColumnCallout = ({ slice }) => {
   return (
     <section className={sty.ColumnCallout}>
-      <div className={sty.copyWrap}>
-        <p className={sty.subtitle}>{slice.primary.subtitle}</p>
-        <PrismicRichText field={slice.primary.title.richText} />
-      </div>
-      <div className={sty.columns}>
-        {slice.items.map((item) => (
-          <div className={sty.employeeCard}>
-            <div className={sty.imageWrap}>
-              <GatsbyImage
-                image={item.image?.gatsbyImageData}
-                alt={item.image?.alt || ''}
-                className={sty.image}
-              />
+      <Container>
+        <div className={sty.copyWrap}>
+          <p className={sty.subtitle}>{slice.primary.subtitle}</p>
+          <PrismicRichText field={slice.primary.title.richText} />
+        </div>
+        <div className={sty.columns}>
+          {slice.items.map((item) => (
+            <div className={sty.employeeCard}>
+              <div className={sty.imageWrap}>
+                <GatsbyImage
+                  image={item.image?.gatsbyImageData}
+                  alt={item.image?.alt || ''}
+                  className={sty.image}
+                />
+              </div>
+              <div className={sty.itemText}>
+                <p>{item.title}</p>
+                <PrismicRichText field={item.description.richText} />
+                <PrismicLink href={item.link} className="BtnPrimary">
+                  {item.link_label}
+                </PrismicLink>
+              </div>
             </div>
-            <div className={sty.itemText}>
-              <p>{item.title}</p>
-              <PrismicRichText field={item.description.richText} />
-              <PrismicLink href={item.link} className="BtnPrimary">
-                {item.link_label}
-              </PrismicLink>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      </Container>
     </section>
   )
 }
@@ -52,6 +55,10 @@ export const query = graphql`
       link_label
       link {
         url
+      }
+      image {
+        gatsbyImageData
+        alt
       }
     }
   }
