f9656822814019d9c8141a24e767b715ee362525
Karlee Milbury <karleemilbury@gmail.com>
2024-09-20 14:19:57 -0300

    Added copy text

--- src/components/slices/column-callout.js
+++ src/components/slices/column-callout.js
@@ -5,7 +5,14 @@ import { PrismicRichText } from '@prismicio/react'
 import * as sty from './column-callout.module.scss'
 
 export const ColumnCallout = ({ slice }) => {
-  return <div></div>
+  return (
+    <section className={sty.ColumnCallout}>
+      <div className={sty.copyWrap}>
+        <p className={sty.subtitle}>{slice.primary.subtitle}</p>
+        <PrismicRichText field={slice.primary.title.richText} />
+      </div>
+    </section>
+  )
 }
 
 export const query = graphql`
