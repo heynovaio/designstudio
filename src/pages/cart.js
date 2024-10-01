545fe5cc43ed3de2668495757ccdf4b15ffd1ad8
Karlee Milbury <karleemilbury@gmail.com>
2024-10-01 14:24:42 -0300

    Added Container

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -23,6 +23,7 @@ import {
   legend,
 } from './cart.module.scss'
 import { getPrice } from '../utils/get-price'
+import { Container } from '../components/Components'
 
 export default function CartPage({ data }) {
   const menu = data.prismicMenu || {}
@@ -42,101 +43,103 @@ export default function CartPage({ data }) {
 
   const isCayman = location.name === 'Cayman Islands'
   return (
-    <Layout menu={menu?.data} title={"Your cart"}>
-      <section className={cart}>
-        <div className={wrap}>
-          {emptyCart ? (
-            <div className={emptyStateContainer}>
-              <h1 className={title}>
-                {isCayman ? 'Your cart is empty' : 'Your wishlist is empty'}
-              </h1>
-              <p>
-                Looks like you haven’t found anything yet. We understand that
-                sometimes it’s hard to choose — maybe this helps:
-              </p>
-            </div>
-          ) : (
-            <>
-              <h1 className={title}>
-                {isCayman ? 'Your cart' : 'Your wishlist'}
-              </h1>
-              <table className={table}>
-                <thead>
-                  <tr className={legend}>
-                    <th className={imageHeader}>Image</th>
-                    <th className={productHeader}>Product</th>
-                    <th className={collapseColumn}>Price</th>
-                    <th>Qty.</th>
-                    <th className={[totals, collapseColumn].join(' ')}>
-                      Total
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {checkout.lineItems.map((item) => (
-                    <LineItem item={item} key={item.id} />
-                  ))}
+    <Layout menu={menu?.data} title={'Your cart'}>
+      <section>
+        <Container>
+          <div className={wrap}>
+            {emptyCart ? (
+              <div className={emptyStateContainer}>
+                <h1 className={title}>
+                  {isCayman ? 'Your cart is empty' : 'Your wishlist is empty'}
+                </h1>
+                <p>
+                  Looks like you haven’t found anything yet. We understand that
+                  sometimes it’s hard to choose — maybe this helps:
+                </p>
+              </div>
+            ) : (
+              <>
+                <h1 className={title}>
+                  {isCayman ? 'Your cart' : 'Your wishlist'}
+                </h1>
+                <table className={table}>
+                  <thead>
+                    <tr className={legend}>
+                      <th className={imageHeader}>Image</th>
+                      <th className={productHeader}>Product</th>
+                      <th className={collapseColumn}>Price</th>
+                      <th>Qty.</th>
+                      <th className={[totals, collapseColumn].join(' ')}>
+                        Total
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {checkout.lineItems.map((item) => (
+                      <LineItem item={item} key={item.id} />
+                    ))}
 
-                  <tr className={summary}>
-                    <td className={collapseColumn}></td>
-                    <td className={collapseColumn}></td>
-                    <td className={collapseColumn}></td>
-                    <td className={labelColumn}>Subtotal</td>
-                    <td className={totals}>{subtotalPrice}</td>
-                  </tr>
-                  <tr className={summary}>
-                    <td className={collapseColumn}></td>
-                    <td className={collapseColumn}></td>
-                    <td className={collapseColumn}></td>
-                    <td className={labelColumn}>Taxes</td>
-                    <td className={totals}>{taxPrice}</td>
-                  </tr>
-                  <tr className={summary}>
-                    <td className={collapseColumn}></td>
-                    <td className={collapseColumn}></td>
-                    <td className={collapseColumn}></td>
-                    <td className={labelColumn}>
-                      {isCayman ? 'Shipping' : ''}
-                    </td>
-                    <td className={totals}>
-                      {isCayman ? 'Calculated at checkout' : ''}
-                    </td>
-                  </tr>
-                  <tr className={grandTotal}>
-                    <td className={collapseColumn}></td>
-                    <td className={collapseColumn}></td>
-                    <td className={collapseColumn}></td>
-                    <td className={labelColumn}>Total Price</td>
-                    <td className={totals}>{totalPrice}</td>
-                  </tr>
-                </tbody>
-              </table>
-              {isCayman ? (
-                <button
-                  onClick={handleCheckout}
-                  disabled={loading}
-                  className="BtnPrimary"
-                  style={{
-                    marginTop: 40,
-                  }}
-                >
-                  Checkout
-                </button>
-              ) : (
-                <button
-                  onClick={()=> window.print()}
-                  disabled={loading}
-                  className="BtnPrimary"
-                  style={{
-                    marginTop: 40,
-                  }}
-                >
-                  Print PDF
-                </button>
-              )}
-            </>
-          )}
-        </div>
+                    <tr className={summary}>
+                      <td className={collapseColumn}></td>
+                      <td className={collapseColumn}></td>
+                      <td className={collapseColumn}></td>
+                      <td className={labelColumn}>Subtotal</td>
+                      <td className={totals}>{subtotalPrice}</td>
+                    </tr>
+                    <tr className={summary}>
+                      <td className={collapseColumn}></td>
+                      <td className={collapseColumn}></td>
+                      <td className={collapseColumn}></td>
+                      <td className={labelColumn}>Taxes</td>
+                      <td className={totals}>{taxPrice}</td>
+                    </tr>
+                    <tr className={summary}>
+                      <td className={collapseColumn}></td>
+                      <td className={collapseColumn}></td>
+                      <td className={collapseColumn}></td>
+                      <td className={labelColumn}>
+                        {isCayman ? 'Shipping' : ''}
+                      </td>
+                      <td className={totals}>
+                        {isCayman ? 'Calculated at checkout' : ''}
+                      </td>
+                    </tr>
+                    <tr className={grandTotal}>
+                      <td className={collapseColumn}></td>
+                      <td className={collapseColumn}></td>
+                      <td className={collapseColumn}></td>
+                      <td className={labelColumn}>Total Price</td>
+                      <td className={totals}>{totalPrice}</td>
+                    </tr>
+                  </tbody>
+                </table>
+                {isCayman ? (
+                  <button
+                    onClick={handleCheckout}
+                    disabled={loading}
+                    className="BtnPrimary"
+                    style={{
+                      marginTop: 40,
+                    }}
+                  >
+                    Checkout
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => window.print()}
+                    disabled={loading}
+                    className="BtnPrimary"
+                    style={{
+                      marginTop: 40,
+                    }}
+                  >
+                    Print PDF
+                  </button>
+                )}
+              </>
+            )}
+          </div>
+        </Container>
       </section>
     </Layout>
   )
